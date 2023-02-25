710fb7908f5ef28bac71b0f8dc924289555517d8
kutaygb <kutaygb@protonmail.com>
2023-02-25 23:40:03 +0300

    download same link fixed

--- imagefetch.py
+++ imagefetch.py
@@ -5,6 +5,7 @@ from constants import headers
 import time
 
 def download_images(image_urls, headers):
+    downloaded_urls = set()
     for url in image_urls:
         page = requests.get(url, headers=headers)
         soup = BeautifulSoup(page.content, "html.parser")
@@ -17,11 +18,13 @@ def download_images(image_urls, headers):
                     continue
                 if not src.endswith(".jpg"):
                     continue
+                if src in downloaded_urls:
+                    continue
                 filename = os.path.join("images", src.split("/")[-1])
                 with open(filename, "wb") as f:
                     response = requests.get(src, headers=headers)
                     f.write(response.content)
                     print(f"{filename} saved.")
                 
-
+                downloaded_urls.add(src)
                 time.sleep(1)
