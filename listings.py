7685cc5c175ba682d1c31a5edb83f938ceec529d
kutaygb <kutaygb@protonmail.com>
2023-02-25 23:56:50 +0300

    Revert "code shorted"
    This reverts commit 3b41f8dd344ad7f517f0d59242e3fef001efd735.

--- listings.py
+++ listings.py
@@ -1,12 +1,18 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+import os
 from constants import headers
 from imagefetch import download_images
 
+page_num = 1
 url = 'https://dolap.com/profil/'
 response = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
-links = [link.get('href') for link in soup.find_all('a', {'rel': 'nofollow', 'href': lambda href: href and 'dolap.com/urun' in href})]
+links = soup.find_all('a', {'rel': 'nofollow', 'href': lambda href: href and 'dolap.com/urun' in href})
 
-download_images(links)
+image_urls = []
+for link in links:
+    image_urls.append(link.get('href'))
+
+download_images(image_urls, headers)
